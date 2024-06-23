3ca3eea3e141037102067d1445b1f77831e20c9b
Mrbysco <Mrbysco@users.noreply.github.com>
2024-06-23 16:19:37 +0200

    Classify Wither Cow as undead

--- src/main/java/gaia/entity/WitherCow.java
+++ src/main/java/gaia/entity/WitherCow.java
@@ -14,6 +14,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -151,6 +152,10 @@ public class WitherCow extends AbstractGaiaEntity {
 	@Override
 	protected void playStepSound(BlockPos pos, BlockState state) {
 		this.playSound(GaiaRegistry.WITHER_COW.getStep(), 0.15F, 1.0F);
+	}
+
+	public MobType getMobType() {
+		return MobType.UNDEAD;
 	}
 
 	@Override
